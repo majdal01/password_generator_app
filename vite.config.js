d37c7e841503b7811a4e26047d0eb0aa5238aff9
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-04-25 08:32:46 +0200

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+    base: '/password_generator_app/',
   css: {
       preprocessorOptions: {
         scss: {
