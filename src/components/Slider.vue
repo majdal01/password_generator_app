1a42182edafe4f593bff212fe291535628352895
Maja Lund Dalgaard <145221922+majdal01@users.noreply.github.com>
2026-04-27 10:24:27 +0200

    Trying to better my code

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -29,7 +29,7 @@
         <div class="sliderlabel-container">
             <h2 id="character-length-label">Character Length</h2>
             <label for="characterLength">
-                <span>{{ modelValue }}</span>
+                <span>{{ props.modelValue }}</span>
             </label>
         </div>
             <input
@@ -37,8 +37,8 @@
                 type="range"
                 aria-labelledby="character-length-label"
                 min="0"
-                :max="sliderMax"
-                :value="modelValue"
+                :max="props.sliderMax"
+                :value="props.modelValue"
                 @input="emit('update:modelValue', Number($event.target.value))"
                 class="custom-slider"
             >
